c9ad1812f623ae21ae3ca511de78cabb99305793
adityacyan <cntsgg@gmail.com>
2025-06-05 18:27:08 +0530

    render deployement fix

--- app.py
+++ app.py
@@ -39,8 +39,9 @@ def home():
     return render_template('index.html', prediction=prediction, emoji=emoji)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
